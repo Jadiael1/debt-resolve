6ed732f6b7b06652ba7b19be0d0a75763d796a16
Jadiael <jadiael@hotmail.com.br>
2023-11-26 20:53:09 -0300

    refactor: NavLink

--- frontend/src/components/atoms/NavLink.tsx
+++ frontend/src/components/atoms/NavLink.tsx
@@ -1,13 +1,21 @@
 import { Anchor } from "./Anchor";
 import ListItems from "./ListItems";
+import { twMerge } from "tailwind-merge";
 
 interface NavLinkProps {
   children: React.ReactNode;
   href: string;
+  merge?: boolean;
+  className?: string;
 }
 
-const NavLink = ({ href, children }: NavLinkProps) => (
-  <ListItems className="text-center">
+const NavLink = ({
+  href,
+  children,
+  merge = false,
+  className = "text-center",
+}: NavLinkProps) => (
+  <ListItems className={merge ? twMerge("text-center") : className}>
     <Anchor
       href={href}
       className="hover:text-gray-300 transition-colors duration-300"
--- frontend/src/components/molecules/NavMenuReduced.tsx
+++ frontend/src/components/molecules/NavMenuReduced.tsx
@@ -1,11 +1,15 @@
 import ListItems from "../atoms/ListItems";
-import NavLinkReduced from "../atoms/NavLinkReduced";
+import NavLink from "../atoms/NavLink";
 import UserDropdownReduced from "./UserDropdownReduced";
 
 const NavMenuReduced = () => (
   <ul className="mb-2 mr-auto flex flex-col pl-0 list-none">
-    <NavLinkReduced href="./">Home</NavLinkReduced>
-    <NavLinkReduced href="./">Contato</NavLinkReduced>
+    <NavLink className="list-item hover:bg-white hover:bg-opacity-10" href="./">
+      Home
+    </NavLink>
+    <NavLink className="list-item hover:bg-white hover:bg-opacity-10" href="./">
+      Contato
+    </NavLink>
     <ListItems className="border-t border-white my-1"></ListItems>
     <UserDropdownReduced />
   </ul>
