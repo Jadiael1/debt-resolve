46d6b67e56e3aa46b8a2cf821ee250bedb86abfb
Jadiael <jadiael@hotmail.com.br>
2023-12-11 06:48:47 -0300

    refactor(Navbar): improve user dropdown functionality

--- frontend/src/components/organisms/Navbar.tsx
+++ frontend/src/components/organisms/Navbar.tsx
@@ -1,26 +1,133 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { FaAngleDown, FaAngleUp, FaSignOutAlt, FaUserCircle } from 'react-icons/fa';
+import ProfilePhoto from '../../assets/user-profile-transparent.png';
 import Logo from '../atoms/Logo';
-import NavButton from '../atoms/NavButton';
-import NavMenuLeft from '../molecules/NavMenuLeft';
-import NavMenuRight from '../molecules/NavMenuRight';
-import NavMenuReduced from '../molecules/NavMenuReduced';
+import NavItem from '../atoms/NavItem';
+import NavigationMenu from '../molecules/NavigationMenu';
+import UserDropdown from '../molecules/UserDropdown';
+import LoginButtonWithIcon from '../molecules/LoginButtonWithIcon';
+import MobileUserDropdown from '../molecules/MobileUserDropdown';
+import MobileControlButtons from '../molecules/MobileControlButtons';
+import UserDropdownSkeleton from '../atoms/UserDropdownSkeleton';
 
 const Navbar = () => {
-	const [toggleMenu, setToggleMenu] = useState<string>('hidden');
-	const handleClickMenu = () => {
-		setToggleMenu(currentValue => (currentValue == 'hidden' ? '' : 'hidden'));
-	};
+	const [isMenuOpen, setIsMenuOpen] = useState(true);
+	const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+	const [isMobileUserDropdownOpen, setIsMobileUserDropdownOpen] = useState(true);
+
+	const navigate = useNavigate();
+	const { user, isLoading } = useAuth();
+	const dropdownRef = useRef<HTMLDivElement>(null);
+	const mobileDropdownRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 768) {
+				// setIsMenuOpen(false);
+				setIsUserDropdownOpen(false);
+			}
+		};
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setIsUserDropdownOpen(false);
+			}
+		};
+		const handleClickOutside = (event: MouseEvent) => {
+			if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+				setIsUserDropdownOpen(false);
+			}
+		};
+		window.addEventListener('resize', handleResize);
+		document.addEventListener('keydown', handleKeyDown);
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+			document.removeEventListener('keydown', handleKeyDown);
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
 	return (
-		<nav className='relative flex items-center py-2 bg-gradient-to-r from-gray-700 to-gray-900 text-white'>
-			<div className='flex w-full px-2 my-auto flex-wrap justify-between'>
-				<Logo />
-				<NavButton handleClickMenu={handleClickMenu} />
-				<div className='hidden sm:flex basis-auto items-center grow transition-all ease-in-out duration-300'>
-					<NavMenuLeft />
-					<NavMenuRight />
+		<nav className='bg-gray-800 text-white shadow-lg'>
+			<div className='container mx-auto flex justify-between items-center py-4 px-6'>
+				<div className='flex items-center'>
+					<Logo />
+					<NavigationMenu defaultClassName />
 				</div>
-				<div className={`${toggleMenu} basis-full grow sm:hidden ml-1 mt-1`}>
-					<NavMenuReduced />
+
+				{user && !isLoading ?
+					<div
+						className='hidden md:flex relative'
+						ref={dropdownRef}
+					>
+						<UserDropdown
+							userName={user.name as string}
+							isDropdownOpen={isUserDropdownOpen}
+							toggleDropdown={() => setIsUserDropdownOpen(!isUserDropdownOpen)}
+						/>
+						<div
+							className={`${
+								isUserDropdownOpen ? 'block' : 'hidden'
+							} absolute right-0 top-full mt-2 w-48 rounded-md shadow-lg py-1 bg-gray-800 ring-1 ring-black ring-opacity-5 z-50 transition-all duration-200 ease-out`}
+						>
+							<NavItem
+								href='/dashboard'
+								icon={() => <FaUserCircle />}
+								className='flex items-center px-3 py-2 rounded-md text-sm font-medium transition duration-300 hover:bg-gray-700'
+							>
+								Dashboard
+							</NavItem>
+							<hr className='my-1 border-gray-700' />
+							<NavItem
+								href='/signout'
+								icon={() => <FaSignOutAlt />}
+								className='flex items-center px-3 py-2 rounded-md text-sm font-medium transition duration-300 hover:bg-gray-700'
+							>
+								Sair
+							</NavItem>
+						</div>
+					</div>
+				: isLoading ?
+					<UserDropdownSkeleton />
+				:	<LoginButtonWithIcon
+						onClick={() => {
+							navigate('/signin');
+						}}
+					/>
+				}
+				<MobileControlButtons
+					isMenuOpen={isMenuOpen}
+					toggleMenu={() => setIsMenuOpen(!isMenuOpen)}
+					navigateToSignIn={() => navigate('/signin')}
+				/>
+			</div>
+			<div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden text-center bg-gray-800`}>
+				<div className='text-center'>
+					{user && !isLoading ?
+						<div
+							className='py-2 border-b border-gray-700'
+							ref={mobileDropdownRef}
+						>
+							<div
+								className='flex items-center justify-center py-2 cursor-pointer select-none'
+								onClick={() => setIsMobileUserDropdownOpen(!isMobileUserDropdownOpen)}
+							>
+								<img
+									className='h-8 w-8 rounded-full select-none'
+									src={ProfilePhoto}
+									alt='Foto do usuário'
+								/>
+								<span className='ml-2 font-semibold text-lg text-white select-none'>{user.name}</span>
+								{isMobileUserDropdownOpen ?
+									<FaAngleUp className='ml-1 h-4 w-4 text-white transition duration-300' />
+								:	<FaAngleDown className='ml-1 h-4 w-4 text-white transition duration-300' />}
+							</div>
+							<MobileUserDropdown isDropdownOpen={isMobileUserDropdownOpen} />
+						</div>
+					:	isLoading && <>Loading...</>}
+					<NavigationMenu />
 				</div>
 			</div>
 		</nav>
