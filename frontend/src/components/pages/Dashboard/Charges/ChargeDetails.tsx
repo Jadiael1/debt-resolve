9aa2210f104be0a3f55ac8de9786efdc68a727d6
Jadiael <jadiael@hotmail.com.br>
2023-12-11 11:32:33 -0300

    feat: implementing adjustments in ChargeDetails.tsx

--- frontend/src/components/pages/Dashboard/Charges/ChargeDetails.tsx
+++ frontend/src/components/pages/Dashboard/Charges/ChargeDetails.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect, FormEvent } from 'react';
 import { useParams } from 'react-router-dom';
-import { FaBan, FaCheck } from 'react-icons/fa';
+import { FaBan, FaCheck, FaEnvelope, FaSpinner } from 'react-icons/fa';
 import { useAuth } from '../../../../contexts/AuthContext';
 import Sidebar from '../../../organisms/Sidebar';
+import Notification from '../../../atoms/Notification';
 import InstallmentUpload from '../../../organisms/InstallmentUpload';
 
 type Charge = {
@@ -34,7 +35,7 @@ type Installment = {
 	amount_paid: string | null;
 	paid: boolean;
 	payment_proof: payment_proof | null;
-	awaiting_approval: boolean | number;
+	awaiting_approval: boolean;
 	user_id: number | null;
 	charge_id: number;
 };
@@ -44,8 +45,15 @@ const ChargeDetails = () => {
 	const [charge, setCharge] = useState<Charge | null>(null);
 	const [installments, setInstallments] = useState<Installment[]>([]);
 	const [loading, setLoading] = useState(true);
+	const [loadingInvite, setLoadingInvite] = useState(false);
+	const [messageInvited, setMessageInvited] = useState<{ message: string; type: 'success' | 'error' | '' }>({
+		message: '',
+		type: '',
+	});
+	const [notification, setNotification] = useState({ message: '', type: '' });
 	const { token, user } = useAuth();
 	// const navigate = useNavigate();
+
 	useEffect(() => {
 		const fetchChargeDetails = async () => {
 			const chargeResponse = await fetch(`https://api.debtscrm.shop/api/v1/charges/${chargeId}/charge`, {
@@ -66,55 +74,165 @@ const ChargeDetails = () => {
 
 			setLoading(false);
 		};
-
 		fetchChargeDetails();
 	}, [chargeId, token]);
 
-	// Functions to handle upload proof, request approval, accept payment, etc.
+	const showNotification = (message: string, type: string) => {
+		setNotification({ message, type });
+	};
+
+	const clearNotification = () => {
+		setNotification({ message: '', type: '' });
+	};
 
 	const handleConfirmPayment = async (installmentId: number, accept: boolean) => {
 		const myHeaders = new Headers();
 		myHeaders.append('Authorization', `Bearer ${token}`);
 		myHeaders.append('Content-Type', 'application/json');
-		const response = await fetch(
-			`https://api.debtscrm.shop/api/v1/installments/${installmentId}/charge/${chargeId}/accept-payment-approval-by-collector`,
-			{
-				method: 'POST',
-				headers: myHeaders,
-				body: JSON.stringify({
-					accept,
-				}),
-				redirect: 'follow',
-			},
-		);
-		const data = await response.json();
-		console.log(data);
+		try {
+			const response = await fetch(
+				`https://api.debtscrm.shop/api/v1/installments/${installmentId}/charge/${chargeId}/accept-payment-approval-by-collector`,
+				{
+					method: 'POST',
+					headers: myHeaders,
+					body: JSON.stringify({
+						accept,
+					}),
+					redirect: 'follow',
+				},
+			);
+			const data = await response.json();
+			if (response.ok && response.status === 200 && data.message === 'Payment marked as paid successfully') {
+				showNotification('Pagamento de parcela confirmado com sucesso!', 'success');
+				setInstallments(prevInstallments =>
+					prevInstallments.map(installment =>
+						installment.id === installmentId ?
+							{ ...installment, paid: true, user_id: user?.id as number }
+						:	installment,
+					),
+				);
+			}
+			if (response.ok && response.status === 200 && data.message === 'Payment successfully declined') {
+				showNotification('Pagamento de parcela recusado com sucesso!', 'error');
+				setInstallments(prevInstallments =>
+					prevInstallments.map(installment =>
+						installment.id === installmentId ? { ...installment, awaiting_approval: false } : installment,
+					),
+				);
+			}
+		} catch (error) {
+			showNotification('Erro inesperado.', 'error');
+		}
 	};
 
 	const handleInviteUser = async (email: string) => {
-		const response = await fetch('https://api.debtscrm.shop/api/v1/charge-invitations/invitations', {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${token}`,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ email, charge_id: chargeId }),
-		});
-		const data = await response.json();
-		console.log(data);
+		setLoadingInvite(true);
+		try {
+			const response = await fetch('https://api.debtscrm.shop/api/v1/charge-invitations/invitations', {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ email, charge_id: chargeId }),
+			});
+			const data = await response.json();
+			if (response.status === 400 && data.message === 'You already participate in this charge') {
+				showNotification('Você não pode convidar a sí mesmo para uma cobrança', 'error');
+				setMessageInvited({
+					message: 'Você não pode convidar a sí mesmo para uma cobrança',
+					type: 'error',
+				});
+			}
+			if (
+				response.status === 429 &&
+				data.message ===
+					'Invitation limit for this email and billing exceeded. Wait a week before sending a new invitation'
+			) {
+				showNotification(
+					'Limite de convites para este e-mail excedido. Espere uma semana antes de enviar um novo convite',
+					'error',
+				);
+				setMessageInvited({
+					message: 'Limite de convites para este e-mail excedido. Espere uma semana antes de enviar um novo convite',
+					type: 'error',
+				});
+			}
+			if (response.status === 500 && data.message === 'Unexpected error when creating resource') {
+				showNotification('Erro inesperado ao criar recurso', 'error');
+				setMessageInvited({
+					message: 'Erro inesperado ao criar recurso',
+					type: 'error',
+				});
+			}
+			if (
+				response.ok &&
+				response.status === 201 &&
+				data.message === 'Invitation to register and participate in billing sent successfully'
+			) {
+				showNotification(
+					'Convite enviado com sucesso, aguarde até que o participante aceite este convite. Volte novamente mais tarde.',
+					'success',
+				);
+				setMessageInvited({
+					message:
+						'Convite enviado com sucesso, aguarde até que o participante aceite este convite. Volte novamente mais tarde.',
+					type: 'success',
+				});
+			}
+		} catch (error) {
+			showNotification('Erro inesperado.', 'error');
+		}
+		setLoadingInvite(false);
 	};
 
 	const sendInstallmentPaymentForApproval = async (installmentId: number) => {
-		const request = await fetch(`https://api.debtscrm.shop/api/v1/installments/send-payment/${installmentId}`, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${token}`,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ charge_id: charge?.id }),
-		});
-		const response = await request.json();
-		console.log(response);
+		try {
+			const response = await fetch(`https://api.debtscrm.shop/api/v1/installments/send-payment/${installmentId}`, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ charge_id: charge?.id }),
+			});
+			const data = await response.json();
+			if (
+				response.ok &&
+				response.status === 200 &&
+				data.message === 'Payment of the installment of the charge sent for analysis successfully'
+			) {
+				showNotification('Pagamento da parcela enviado para análise com sucesso', 'success');
+				setInstallments(prevInstallments =>
+					prevInstallments.map(installment =>
+						installment.id === installmentId ? { ...installment, awaiting_approval: true } : installment,
+					),
+				);
+			}
+			if (response.status === 409 && data.message === 'This installment is already under payment approval analysis') {
+				showNotification('Esta parcela já está em análise de aprovação de pagamento', 'error');
+			}
+			if (
+				response.status === 422 &&
+				data.message === 'Before sending the payment for analysis you need to send proof of payment'
+			) {
+				showNotification(
+					'Antes de enviar o pagamento para análise é necessário enviar o comprovante de pagamento',
+					'error',
+				);
+			}
+			if (
+				response.status === 403 &&
+				data.message === 'You cannot send payment for an installment of a charge for which you are not the debtor'
+			) {
+				showNotification(
+					'Você não pode enviar o pagamento de uma parcela de uma cobrança do qual você não é o devedor',
+					'error',
+				);
+			}
+		} catch (error) {
+			showNotification('Erro inesperado.', 'error');
+		}
 	};
 
 	const isOnlyParticipant =
@@ -123,9 +241,17 @@ const ChargeDetails = () => {
 			(charge.debtor_id === user?.id && !charge.collector_id));
 
 	const oppositeRole = charge && charge.collector_id === parseInt(user?.id as string) ? 'Devedor' : 'Cobrador';
+	const myRole = charge && charge.collector_id === parseInt(user?.id as string) ? 'Cobrador' : 'Devedor';
 
 	return (
 		<Sidebar>
+			{notification.message && (
+				<Notification
+					message={notification.message}
+					type={notification.type}
+					onClose={clearNotification}
+				/>
+			)}
 			{isOnlyParticipant ?
 				<div className='mb-6 p-4 border rounded'>
 					<h3 className='text-lg font-semibold mb-2 text-center'>Convidar Participante</h3>
@@ -150,31 +276,58 @@ const ChargeDetails = () => {
 						/>
 						<button
 							type='submit'
-							className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded'
+							className={`${
+								loadingInvite ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-700'
+							} ml-2 text-white font-bold py-1 px-2 rounded`}
+							disabled={loadingInvite ? true : false}
 						>
-							Enviar Convite
+							<span className='flex items-center'>
+								Enviar Convite
+								<FaEnvelope className='ml-2' />
+								{loadingInvite && <FaSpinner className='animate-spin ml-2' />}
+							</span>
 						</button>
 					</form>
+					{messageInvited.message && (
+						<p
+							className={`mb-2 text-center mt-2 ${
+								messageInvited.type === 'success' ? 'bg-green-500' : 'bg-yellow-500'
+							}  rounded text-white font-medium`}
+						>
+							{messageInvited.message}
+						</p>
+					)}
 				</div>
 			:	<div className='container mx-auto p-4'>
 					<h1 className='text-2xl font-semibold mb-4'>Detalhes da Cobrança</h1>
 					{loading ?
 						<div>Carregando...</div>
 					:	<>
-							<div className='mb-6'>
-								<h2 className='text-xl font-bold mb-2'>Informações da Cobrança</h2>
-								<p>
-									<strong>Nome:</strong> {charge?.name}
-								</p>
-								<p>
-									<strong>Descrição:</strong> {charge?.description}
-								</p>
-								<p>
-									<strong>Valor Total:</strong> R$ {charge?.amount}
-								</p>
-								<p>
-									<strong>Debito Restante:</strong> R$ {charge?.amount_paid}
-								</p>
+							<div className='flex justify-between flex-wrap'>
+								<div className='mb-6'>
+									<h2 className='text-xl font-bold mb-2'>Informações da Cobrança</h2>
+									<p>
+										<strong>Nome:</strong> {charge?.name}
+									</p>
+									<p>
+										<strong>Descrição:</strong> {charge?.description}
+									</p>
+									<p>
+										<strong>Valor Total:</strong> R$ {charge?.amount}
+									</p>
+									<p>
+										<strong>Debito Restante:</strong> R$ {charge?.amount_paid}
+									</p>
+								</div>
+
+								<div className='mb-6'>
+									<p>
+										<strong>Criação da cobrança: </strong> {(charge?.created_at as string).substring(0, 19)}
+									</p>
+									<p>
+										<strong>Minha função: </strong> {myRole}
+									</p>
+								</div>
 							</div>
 
 							{user?.id === charge?.collector_id &&
@@ -235,18 +388,19 @@ const ChargeDetails = () => {
 										.map(installment => (
 											<div
 												key={installment.id}
-												className='mb-4 p-4 border rounded flex justify-between flex-wrap'
+												className='mb-4 p-4 border rounded md:flex md:justify-between md:flex-wrap'
 											>
-												<div className='block break-all'>
+												<div className='block break-all text-center'>
 													<p>
 														Parcela {installment.installment_number}: R$ {installment.value}
 													</p>
 													<p>Vencimento: {installment.due_date}</p>
 												</div>
-												<div>
+												<div className='mb-2 mt-2 flex justify-center items-center'>
 													{installment.payment_proof && (
 														<p>
 															<img
+																className=''
 																src={`${installment.payment_proof.path}`}
 																alt='comprovante'
 																width={'128px'}
@@ -259,24 +413,57 @@ const ChargeDetails = () => {
 														<InstallmentUpload
 															installmentId={installment.id}
 															chargeId={charge?.id as number}
+															showNotification={showNotification}
+															setInstallments={setInstallments}
 														/>
-													:	<button
-															type='button'
-															className='bg-green-700 hover:bg-green-500 text-white font-bold px-2 rounded flex items-center justify-center flex-wrap text-sm'
-															onClick={() => {
-																sendInstallmentPaymentForApproval(installment.id);
-															}}
-														>
-															Enviar Pagamento Para Analise
-														</button>)}
+													:	<div className='flex items-center justify-center'>
+															<button
+																type='button'
+																className='xs:bg-green-700 xs:hover:bg-green-500 xs:text-white xs:font-bold xs:p-2 xs:rounded xs:text-sm text-xs px-0 py-1 bg-green-700 hover:bg-green-500 rounded text-white'
+																onClick={() => {
+																	sendInstallmentPaymentForApproval(installment.id);
+																}}
+															>
+																<span>Enviar Pagamento Para Analise</span>
+															</button>
+														</div>)}
 											</div>
 										))}
 								</div>
 							</div>
-							{/* List installments */}
 
-							{/* If user is debtor, show upload button */}
-							{/* If user is collector, show payments awaiting approval */}
+							<div>
+								<h2 className='text-xl font-bold mb-2 text-green-400'>Parcelas Pagas</h2>
+								<div>
+									{installments
+										.filter(installment => installment.paid && installment.awaiting_approval)
+										.map(installment => (
+											<div
+												key={installment.id}
+												className='mb-4 p-4 border border-green-400 border-2 rounded md:flex md:justify-between md:flex-wrap'
+											>
+												<div className='block break-all text-center'>
+													<p>
+														Parcela {installment.installment_number}: R$ {installment.value}
+													</p>
+													<p>Vencimento: {installment.due_date}</p>
+												</div>
+												<div className='mb-2 mt-2 flex justify-center items-center'>
+													{installment.payment_proof && (
+														<p>
+															<img
+																className=''
+																src={`${installment.payment_proof.path}`}
+																alt='comprovante'
+																width={'128px'}
+															/>
+														</p>
+													)}
+												</div>
+											</div>
+										))}
+								</div>
+							</div>
 						</>
 					}
 				</div>
