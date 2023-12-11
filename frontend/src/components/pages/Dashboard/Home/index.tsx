6ed2417d9a9825aa6a51f3348ee0eb4ca2a6636d
Jadiael <jadiael@hotmail.com.br>
2023-12-11 06:44:28 -0300

    refactor: Remove unused code and update component structure

--- frontend/src/components/pages/Dashboard/Home/index.tsx
+++ frontend/src/components/pages/Dashboard/Home/index.tsx
@@ -1,88 +1,31 @@
-import { useState } from 'react';
-import { useAuth } from '../../../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import Sidebar from '../../../organisms/Sidebar';
 
 function Dashboard() {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-	const { logout } = useAuth();
-	const toggleSidebar = () => {
-		setIsSidebarOpen(!isSidebarOpen);
-	};
+	const navigate = useNavigate();
 
 	return (
-		<div className='bg-gray-100 min-h-screen'>
-			{/* Navbar */}
-			<nav className='bg-white shadow-lg p-4 flex justify-between items-center'>
-				{/* Botão de Toggle para Sidebar em Telas Menores */}
-				<button
-					onClick={toggleSidebar}
-					className='text-xl font-bold md:hidden'
+		<Sidebar>
+			<h1 className='text-2xl font-semibold mb-6'>Bem-vindo ao Painel de Controle</h1>
+
+			<div className='grid md:grid-cols-2 gap-4'>
+				<div
+					className='bg-white rounded-lg shadow p-6 hover:shadow-md transition duration-300 ease-in-out cursor-pointer'
+					onClick={() => navigate('/dashboard/charge/create')}
 				>
-					{/* Ícone do Menu (Exemplo: ícone de hambúrguer) */}☰
-				</button>
-
-				<div className='text-xl font-bold'>MyApp</div>
-
-				{/* Informações do Usuário */}
-				<div className='flex items-center'>
-					<span className='mr-2'>Olá, Usuário</span>
-					<button
-						className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-						onClick={logout}
-					>
-						Sair
-					</button>
+					<h2 className='text-lg font-semibold'>Criar Cobrança</h2>
+					<p className='text-gray-600 mt-2'>Inicie o processo para criar uma nova cobrança.</p>
 				</div>
-			</nav>
 
-			{/* Sidebar */}
-			<div
-				className={`absolute z-20 w-64 bg-gray-800 min-h-screen text-white transform ${
-					isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-				} transition-transform duration-300 ease-in-out md:relative md:translate-x-0`}
-			>
-				<ul className='p-4'>
-					<li className='mb-4'>
-						<a
-							href='/dashboard'
-							className='hover:text-gray-200'
-						>
-							Dashboard
-						</a>
-					</li>
-					<li className='mb-4'>
-						<a
-							href='/cobrancas'
-							className='hover:text-gray-200'
-						>
-							Cobranças
-						</a>
-					</li>
-					<li className='mb-4'>
-						<a
-							href='/parcelas'
-							className='hover:text-gray-200'
-						>
-							Parcelas
-						</a>
-					</li>
-					<li className='mb-4'>
-						<a
-							href='/perfil'
-							className='hover:text-gray-200'
-						>
-							Perfil
-						</a>
-					</li>
-					{/* Mais itens conforme necessário */}
-				</ul>
+				<div
+					className='bg-white rounded-lg shadow p-6 hover:shadow-md transition duration-300 ease-in-out cursor-pointer'
+					onClick={() => navigate('/dashboard/charges')}
+				>
+					<h2 className='text-lg font-semibold'>Cobranças Ativas</h2>
+					<p className='text-gray-600 mt-2'>Veja suas cobranças ativas e gerencie-as.</p>
+				</div>
 			</div>
-
-			{/* Conteúdo Principal */}
-			<main className={`p-7 ${isSidebarOpen ? 'ml-64' : ''} md:ml-64`}>
-				<h1 className='text-2xl font-semibold'>Bem-vindo ao Painel de Controle</h1>
-				{/* Outros componentes e conteúdo conforme necessário */}
-			</main>
-		</div>
+		</Sidebar>
 	);
 }
 
