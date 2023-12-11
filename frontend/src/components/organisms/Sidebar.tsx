26a9b69de8f8dce2820d65b617d36587718482e7
Jadiael <jadiael@hotmail.com.br>
2023-12-11 06:49:01 -0300

    feat: Add Sidebar component

---
+++ frontend/src/components/organisms/Sidebar.tsx
@@ -0,0 +1,172 @@
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import {
+	FaSignOutAlt,
+	FaPlus,
+	FaList,
+	FaHome,
+	FaBars,
+	FaTimes,
+	FaUserCircle,
+	FaCaretDown,
+	FaCaretRight,
+	FaMoneyBillWave,
+} from 'react-icons/fa';
+
+import ProfilePhoto from '../../assets/user-profile-transparent.png';
+import NavItem from '../atoms/NavItem';
+
+type TSidebarProps = {
+	children: React.ReactNode;
+};
+
+const Dashboard = ({ children }: TSidebarProps) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isChargesSubmenuOpen, setIsChargesSubmenuOpen] = useState(true);
+	const { user } = useAuth();
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
+
+	const toggleChargesSubmenu = () => {
+		setIsChargesSubmenuOpen(!isChargesSubmenuOpen);
+	};
+
+	const isActive = (path: string) => {
+		// return location.pathname === path ? 'opacity-100' : 'opacity-50';
+		return location.pathname.startsWith(path) ? 'opacity-100' : 'opacity-50';
+	};
+
+	return (
+		<div className='bg-gray-100 min-h-screen'>
+			{/* Navbar */}
+			<nav className='bg-gray-800 shadow-lg p-4 flex justify-between items-center sticky top-0 border-b-2 border-white'>
+				<div className='flex items-center'>
+					<button
+						onClick={toggleSidebar}
+						className='text-xl font-bold md:hidden mr-2'
+					>
+						{isSidebarOpen ?
+							<FaTimes className='fill-white' />
+						:	<FaBars className='fill-white' />}
+					</button>
+					<div
+						onClick={() => navigate('/')}
+						className='flex items-center cursor-pointer rounded border px-1 hover:bg-gray-900'
+					>
+						<FaHome className='text-xl fill-white' />
+						<span className='ml-1 font-bold text-white'>Início</span>
+					</div>
+				</div>
+				<div className='text-xl font-bold text-white'>DebtsCRM</div>
+			</nav>
+
+			{/* Sidebar */}
+			<div
+				className={`fixed z-20 w-64 bg-gray-800 min-h-screen text-white transform ${
+					isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+				} transition-transform duration-300 ease-in-out md:translate-x-0`}
+			>
+				{/* ...User profile content... */}
+				<div className='bg-gray-700 p-4 text-center'>
+					<img
+						src={ProfilePhoto}
+						alt='Foto do usuário'
+						className='h-12 w-12 rounded-full mx-auto'
+					/>
+					<h3 className='mt-2 font-semibold'>{user?.name}</h3>
+				</div>
+				{/* ...End User Profile Content... */}
+				<ul className='p-4'>
+					{/* cursor-pointer hover:text-gray-200 hover:bg-gray-700 rounded */}
+					<li className={`${isActive('/dashboard')} mb-6`}>
+						<div
+							onClick={() => navigate('/dashboard')}
+							className='flex items-center justify-center py-2 cursor-pointer hover:text-gray-200'
+						>
+							<FaUserCircle className='mr-2' />
+							<span>Dashboard</span>
+						</div>
+					</li>
+					{/* */}
+					<li className='mb-2'>
+						<div
+							className={`${isActive(
+								'/dashboard/charge',
+							)} flex items-center justify-between py-2 cursor-pointer hover:bg-gray-700 rounded`}
+							onClick={toggleChargesSubmenu}
+						>
+							<span className='flex items-center'>
+								<FaMoneyBillWave className='mr-2' />
+								Cobranças
+							</span>
+							{isChargesSubmenuOpen ?
+								<FaCaretDown />
+							:	<FaCaretRight />}
+						</div>
+						{isChargesSubmenuOpen && (
+							<ul className='pl-4'>
+								<li
+									className={`${isActive(
+										'/dashboard/charge/create',
+									)} cursor-pointer hover:text-gray-200 hover:bg-gray-700`}
+								>
+									<div
+										onClick={() => navigate('/dashboard/charge/create')}
+										className='py-1 flex items-center'
+									>
+										<FaPlus className='mr-2' />
+										Criar Nova Cobrança
+									</div>
+								</li>
+								<li
+									className={`${isActive('/dashboard/charges')} cursor-pointer hover:text-gray-200 hover:bg-gray-700`}
+								>
+									<div
+										onClick={() => navigate('/dashboard/charges')}
+										className='py-1 flex items-center'
+									>
+										<FaList className='mr-2' />
+										Suas Cobranças
+									</div>
+								</li>
+							</ul>
+						)}
+					</li>
+					{/* */}
+					<li
+						className={`${isActive(
+							'/dashboard/perfil',
+						)} mb-2 cursor-pointer hover:text-gray-200 hover:bg-gray-700 rounded`}
+					>
+						<div
+							onClick={() => navigate('/dashboard/perfil')}
+							className='flex items-center py-2'
+						>
+							<FaUserCircle className='mr-2' />
+							<span>Perfil</span>
+						</div>
+					</li>
+					<li className={`cursor-pointer rounded mt-6`}>
+						<NavItem
+							href='/signout'
+							className='flex items-center hover:text-red-900 justify-center hover:bg-gray-700'
+							noSpan
+						>
+							<FaSignOutAlt className='mr-2 fill-red-900' />
+							Sair
+						</NavItem>
+					</li>
+				</ul>
+			</div>
+
+			<main className={`p-7 md:ml-64 transition-all duration-300 ease-in-out`}>{children}</main>
+		</div>
+	);
+};
+
+export default Dashboard;
